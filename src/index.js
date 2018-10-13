429ba1fea9e5e20751ecc2c52c641ebadcce6ccc
Veronica Bustabad <vbustabad@gmail.com>
2018-10-13 03:28:08 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ class MovieCard extends React.Component {
 }
 
 const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
- 
+
 ReactDOM.render(
   <MovieCard
     title="Jurassic World"
     poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
-    genres={jurassicWorldGenres} 
+    genres={jurassicWorldGenres}
   />,
   document.getElementById('root')
 );
