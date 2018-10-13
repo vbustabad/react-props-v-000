940615757a99b5046d5a3b96a00b4a6d200047f9
Veronica Bustabad <vbustabad@gmail.com>
2018-10-13 03:24:05 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ class MovieCard extends React.Component {
   }
 }
 
-const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
- 
-ReactDOM.render(
-  <MovieCard 
-    title="Mad Max: Fury Road"
-    poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
-    genres={madMaxGenres}
-  />,
-  document.getElementById('root')
-);
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
